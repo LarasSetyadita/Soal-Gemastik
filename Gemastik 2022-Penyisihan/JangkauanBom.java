d7487f06c6c490f79c7670d1c80a8146146694ce
rasLarase <dwilaras.2022@student.uny.ac.id>
2024-07-31 13:41:06 +0700

    JangkauanBom sudah benar

--- Gemastik 2022-Penyisihan/JangkauanBom.java
+++ Gemastik 2022-Penyisihan/JangkauanBom.java
@@ -1,37 +1,43 @@
 import java.util.Arrays;
 import java.io.*;
 import java.util.StringTokenizer;
+
 public class JangkauanBom {
    static class FastReader {
       BufferedReader br;
       StringTokenizer st;
-     
-      public FastReader(){
-         br = new BufferedReader(
-         new InputStreamReader(System.in));
+
+      public FastReader() {
+         br = new BufferedReader(new InputStreamReader(System.in));
       }
-     
-      String next(){
+
+      String next() {
          while (st == null || !st.hasMoreElements()) {
-             try {
-                st = new StringTokenizer(br.readLine());
-             } catch (IOException e) {
-                e.printStackTrace();
-             }
+            try {
+               st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
          }
          return st.nextToken();
       }
-     
-      int nextInt() { return Integer.parseInt(next()); }
-     
-      long nextLong() { return Long.parseLong(next()); }
-     
-      double nextDouble() {return Double.parseDouble(next()); }
-     
+
+      int nextInt() {
+         return Integer.parseInt(next());
+      }
+
+      long nextLong() {
+         return Long.parseLong(next());
+      }
+
+      double nextDouble() {
+         return Double.parseDouble(next());
+      }
+
       String nextLine() {
          String str = "";
          try {
-            if(st.hasMoreTokens()) {
+            if (st.hasMoreTokens()) {
                str = st.nextToken("\n");
             } else {
                str = br.readLine();
@@ -42,33 +48,49 @@ public class JangkauanBom {
          return str;
       }
    }
+
    public static void main(String[] args) {
       FastReader console = new FastReader();
-      int xa = console.nextInt();
-      int ya = console.nextInt();
+      StringBuilder output = new StringBuilder();
+      long xa = console.nextInt();
+      long ya = console.nextInt();
       int M = console.nextInt();
-      double [] musuh = new double [M];
+      long[] musuh = new long[M];
+
       for (int i = 0; i < M; i++) {
          int x = console.nextInt();
          int y = console.nextInt();
-         musuh [i]  = Math.pow((double)(x - xa), 2) + Math.pow((double) (y - ya), 2);      
+         musuh[i] = radiusLawan(xa, ya, x, y);
       }
+
       Arrays.sort(musuh);
+
       int Q = console.nextInt();
-      int [] h = new int [Q];
-      for (int i = 0; i < Q; i++) {
-         int a = console.nextInt();
-         double b = Math.pow(a, 2);
-         h[i] = ledakan(musuh, b);
+      for (int j = 0; j < Q; j++) {
+         long b = console.nextLong();
+         // menyimpan hasil ke string builder sebagai string "n" sebagai string tambahan
+         output.append(ledakan(musuh, b * b)).append("\n");
       }
-      for (int i = 0; i < Q; i++) {
-         System.out.println(h[i]);
-      }
+      System.out.print(output.toString());
+
    }
-   public static int ledakan(double[] musuh, double b) {
-      for (int i = 0; i < musuh.length; i++) {
-         if (musuh[i] > b) return i;       
+
+   public static int ledakan(long[] musuh, double b) {
+      int left = 0;
+      int right = musuh.length - 1;
+      while (left <= right) {
+         int mid = left + (right - left) / 2;
+         if (musuh[mid] > b) {
+            right = mid - 1;
+         } else {
+            left = mid + 1;
+         }
       }
-      return musuh.length;
+      return left;
    }
+
+   public static long radiusLawan(long x1, long y1, long x2, long y2) {
+      return (long) ((x2 - x1) * (x2 - x1)) + (long) ((y2 - y1) * (y2 - y1));
+   }
+
 }
